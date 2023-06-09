3e0f16b7e51cb6cb298bd0f2fd9e2072cdcc7527
muthuviswanath <muthuviswanath@outlook.com>
2023-06-09 15:29:38 +0530

    second

--- src/com/rakuten/Assignment.java
+++ src/com/rakuten/Assignment.java
@@ -3,5 +3,11 @@ package com.rakuten;
 public class Assignment {
 private int id;
 private String name;
+public Assignment(int id, String name) {
+	super();
+	this.id = id;
+	this.name = name;
+}
+
 
 }
